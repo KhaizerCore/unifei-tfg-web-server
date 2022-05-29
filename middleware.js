54a7e2b8f55451ccb84a6bde07aaab00c9aa4c17
KhaizerCore <gustavo.werkhaizer@hotmail.com>
2022-05-29 18:50:10 -0300

    CORS resolved. Allow all origins.

--- middleware.js
+++ middleware.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use(cors());
+
 
 
 // parameters( path, function(request, response, nextFunction))
